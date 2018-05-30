c11d92e57e4fccfad0bc58ca1f43f18391acb188
Jonatha Azevedo da Costa <jonatha.costa@fgv.br>
2018-05-30 16:46:21 -0300

    up

--- elasticsearch/getnews_from_elastic_to_classificator.py
+++ elasticsearch/getnews_from_elastic_to_classificator.py
@@ -7,6 +7,7 @@ Created on Wed Mar 14 16:02:43 2018
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
 from pymongo import MongoClient
+from pandas.io.json import json_normalize
 import pandas as pd 
 import numpy as np
 import requests
@@ -17,7 +18,9 @@ os.chdir('elasticsearch')
 credenciais = pd.read_table('credencias.txt')
 es = Elasticsearch(str(credenciais['x'][0]))
 
-results = json.loads(r.text)
+
+
+df = pd.DataFrame()
 
 page = es.search(
   index = 'extra',
@@ -33,7 +36,7 @@ page = es.search(
 sid = page['_scroll_id']
 scroll_size = page['hits']['total']
   
-Frame = pd.DataFrame()
+
   # Start scrolling
 while (scroll_size > 0):
    print("Scrolling...")
@@ -42,7 +45,8 @@ while (scroll_size > 0):
    sid = page['_scroll_id']
    # Get the number of results that we returned in the last scroll
    scroll_size = len(page['hits']['hits'])
-   kaux = pd.DataFrame(page['hits']['hits'])
-   Frame = Frame.append(pd.DataFrame(data = kaux), ignore_index=True)
+   kaux =page['hits']['hits']
+   df_aux = json_normalize(page['hits']['hits'])
+   df =df.append(df_aux,ignore_index = True)
    print("scroll size: " + str(scroll_size))
    # Do something with the obtained page
